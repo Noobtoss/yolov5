3b189dc1b5810e35e4130f587ef7d91cd9eca3f1
Noobtoss <thomasschmittba17196@yahoo.de>
2023-01-18 18:36:33 +0100

    Wed Jan 18 18:36:24 CET 2023

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -40,21 +40,21 @@ class Albumentations:
                     A.RandomGamma(p=0.0),
                     A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
                 
-                self.transform = [A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))]
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
             
             if version == 1:
                 
-                T = [ A.Blur(p=0.00)]
+                T = [A.Blur(p=0.00)]
                 
-                self.transform = [A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))]
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
             
             if version == 2:
                 
-                T1 = [  A.Equalize(p=0.2)]
-                T2 = [  A.RGBShift(p=0.2, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
-                T3 = [  A.HueSaturationValue(p=0.2, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
-                T4 = [  A.ISONoise(p=0.2)]
-                T5 = [  A.RandomBrightnessContrast(p=0.2, brightness_limit=0.3, contrast_limit=0.3)]
+                T1 = [A.Equalize(p=0.2)]
+                T2 = [A.RGBShift(p=0.2, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
+                T3 = [A.HueSaturationValue(p=0.2, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
+                T4 = [A.ISONoise(p=0.2)]
+                T5 = [A.RandomBrightnessContrast(p=0.2, brightness_limit=0.3, contrast_limit=0.3)]
                 
                 self.transform = [
                             A.Compose(T1, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
@@ -67,11 +67,11 @@ class Albumentations:
 
             if version == 3:
                  
-                T1 = [  A.Equalize(p=0.2)]
-                T2 = [  A.RGBShift(p=0.4, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
-                T3 = [  A.HueSaturationValue(p=0.4, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
-                T4 = [  A.ISONoise(p=0.2)]
-                T5 = [  A.RandomBrightnessContrast(p=0.2, brightness_limit=0.3, contrast_limit=0.3)]
+                T1 = [A.Equalize(p=0.2)]
+                T2 = [A.RGBShift(p=0.4, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
+                T3 = [A.HueSaturationValue(p=0.4, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
+                T4 = [A.ISONoise(p=0.2)]
+                T5 = [A.RandomBrightnessContrast(p=0.2, brightness_limit=0.3, contrast_limit=0.3)]
             
                 self.transform = [
                             A.Compose(T1, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
@@ -94,11 +94,11 @@ class Albumentations:
                     A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=0.5),
                     A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=0.5)]
 
-                T1 = [  A.Equalize(p=0.1)]
-                T2 = [  A.RGBShift(p=0.2, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
-                T3 = [  A.HueSaturationValue(p=0.2, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
-                T4 = [  A.ISONoise(p=0.1)]
-                T5 = [  A.RandomBrightnessT0+T+Contrast(p=0.1, brightness_limit=0.3, contrast_limit=0.3)]
+                T1 = [A.Equalize(p=0.1)]
+                T2 = [A.RGBShift(p=0.2, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
+                T3 = [A.HueSaturationValue(p=0.2, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
+                T4 = [A.ISONoise(p=0.1)]
+                T5 = [A.RandomBrightnessT0+T+A.Contrast(p=0.1, brightness_limit=0.3, contrast_limit=0.3)]
 
                 self.transform = [
                             A.Compose(T0+T+T1, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels'])),
@@ -115,7 +115,19 @@ class Albumentations:
                     A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=1),
                     A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=1)]
 
-                self.transform = [A.Compose(T, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))]
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))
+
+            if version == 6:
+
+                T_pre = [A.CoarseDropout(max_holes=20, max_height=size*0.05, max_width=size*0.05, min_holes=10, min_height=size*0.01, min_width=size*0.01, fill_value=0, p=0.6)]
+
+                T = [
+                    A.PixelDropout(dropout_prob=0.05, per_channel=False, drop_value=0, p=0.6),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=1),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=1)]
+
+                self.transform_pre = A.Compose(T_pre)
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))
 
             LOGGER.info(prefix + ', '.join(f'{x}'.replace('always_apply=False, ', '') for x in T if x.p))
         except ImportError:#  # package not installed, skip
@@ -124,9 +136,21 @@ class Albumentations:
             LOGGER.info(f'{prefix}{e}')
 
     def __call__(self, im, labels, p=1.0):
-        if self.transform and random.random() < p:
-            new = random.choice(self.transform)(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
-            im, labels = new['image'], np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
+
+        if random.random() < p:
+
+            if self.transform_pre:
+                new = self.transform_pre(image=im) # transformed
+                im = new['image']
+
+            if self.transform:
+
+                if isinstance(self.transform, list):
+                    new = random.choice(self.transform)(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0]) # transformed
+                else:
+                    new = self.transform(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
+                im, labels = new['image'], np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
+
         return im, labels
 
 
